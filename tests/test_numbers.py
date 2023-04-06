b47ccab88e01baec4d2a16ed02a94fb73861a7b1
RachelEJ <rej3713@gmail.com>
2023-04-05 21:55:08 -0500

    Create test_numbers.py

---
+++ tests/test_numbers.py
@@ -0,0 +1,28 @@
+```python
+import math
+import pytest
+import numbers
+
+
+## takes two numbers and returns
+## the result of a division
+def numbers(num1, num2):
+    try:
+        try:
+            temp1 = int(num1)
+            temp2 = int(num2)
+        except:
+            print("num1 and num2 must be numbers")
+            return
+        if num2 == 0:
+            print("NO DIVIDING BY ZERO")
+            return
+        return (num1 / num2)
+    except:
+        print("error processing input.")
+
+@pytest.mark.parametrize("numb1, numb2, quotient", [(4, 2, 2), (7, 3, 3), ("hey", "uh", "no"), (0, 0, 0), ("", "", "")])
+
+def test_numbers(numb1, numb2, quotient):
+    assert numbers(numb1, numb2) == quotient
+```
