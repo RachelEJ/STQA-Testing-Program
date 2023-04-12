9100affced224e0588ca2299462b39b0a5799f43
RachelEJ <rej3713@gmail.com>
2023-04-12 18:42:34 -0500

    Working flask setup
    4 pages. 1 for testing, 1 for form, 1 for results, 1 doesn't work.

--- app/routes.py
+++ app/routes.py
@@ -1,49 +1,47 @@
-# AGENDA: Call CalcBMI on inputs, Call ClassifyBMI on CalcBMI's result
-# requires import of files
-# 
+# NOTE: form.html is not responding
+# /index contains a paragraph tag
+# /test contains the working form
+# /form won't display at all
+# /data contains the correct results
 
 from flask import Flask, render_template, request
+from app.BMI.BMIsystem import CalcBMI, ClassifyBMI
 from app import app
  
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
 @app.route('/form')
 def form():
     return render_template('form.html')
  
-@app.route('/data/', methods = ['POST', 'GET'])
+@app.route('/data', methods = ['POST', 'GET'])
 def data():
     if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+        return "The URL /data is accessed directly. Try going to '/form' to submit form"
     if request.method == 'POST':
         vals = request.form.getlist('BMI_params')
         feet = int(vals[0])
         inches = int(vals[1])
         pounds = int(vals[2])
-        result = feet + inches + pounds
-        return render_template('data.html', feet=feet, inches=inches, pounds=pounds, result=result)
- 
+        result = CalcBMI(feet, inches, pounds)
+        
+        categoryNum = ClassifyBMI(result)
+        if (categoryNum == 1):
+            category = "underweight"
+        elif (categoryNum == 2):
+            category = "normal weight"
+        elif (categoryNum == 3):
+            category = "overweight"
+        elif (categoryNum == 4):
+            category = "obese"
+        else:
+            category = "ERROR"
 
-# from flask import Flask, request, render_template_string
-# from app import app
-
-# @app.route('/', methods=['GET', 'POST'])
-# @app.route('/index', methods=['GET', 'POST'])
-# def index():
-
-#     if request.method == 'POST':
-#         vals = request.form.getlist('daily_calories')
-#         val1 = float(vals[0])
-#         val2 = float(vals[1])
-#         result = val1 + val2
-#     else:
-#         val1 = ''
-#         val2 = ''
-#         result = ''
-
-#     return render_template_string('''
-# <form action="/" method="POST">
-# <p>Input 1: <input type="number" name="daily_calories" value="{{ val1 }}"></p>
-# <p>Input 2: <input type="number" name="daily_calories" value="{{ val2 }}"></p>
-# <input type="submit" value="Calculate">
-# <p>Result: <input type="number" name="result_dc" value="{{ result }}"></p>
-# </form>
-# ''', val1=val1, val2=val2, result=result)
+        return render_template('data.html', feet=feet, inches=inches, pounds=pounds, result=result, category=category)
