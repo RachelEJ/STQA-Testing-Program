0db60b1e8b41ce92eca21269498c540a5f71acdf
RachelEJ <rej3713@gmail.com>
2023-04-13 04:02:05 -0500

    Added comments

--- flaskFolder/app.py
+++ flaskFolder/app.py
@@ -1,3 +1,5 @@
+# File containing Flask webpage setup
+
 from flask import Flask, render_template, request
 from flaskFolder.BMI.BMIsystem import CalcBMI, ClassifyBMI
 
@@ -6,11 +8,17 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index', methods = ['POST', 'GET'])
 
+# HOME PAGE
 def index():
+    # reads data after the POST request
     if request.method == 'POST':
+
+        # stores values from the input elements
         feet = int(request.form.get('feetInput'))
         inches = int(request.form.get('inchesInput'))
         pounds = int(request.form.get('poundsInput'))
+
+        # calculates and classifies BMI
         result = CalcBMI(feet, inches, pounds)
         categoryNum = ClassifyBMI(result)
 
@@ -25,8 +33,10 @@ def index():
         else:
             category = "ERROR"
 
+        # renders page using template and data from above
         return render_template('index.html', feet=feet, inches=inches, pounds=pounds, result=result, category=category)
     else:
+        # renders page using template and no data
         return render_template('index.html')
 
 if __name__ == "__main__":
--- flaskFolder/tests/test_app.py
+++ flaskFolder/tests/test_app.py
@@ -1,8 +1,11 @@
+# File containing E2E tests for Flask webpage
+
 import pytest
 import requests
 from flask import request
 from flaskFolder.app import app
 
+# E2E test that verifies page is up and running
 @pytest.mark.e2e
 def test_page_status(client):
     requests.post(
@@ -14,12 +17,15 @@ def test_page_status(client):
 
     assert response.status_code == 200
 
+# E2E test that verifies page displays correct results given specified input
 @pytest.mark.e2e
 def test_inputs(client):
     
+    # passes in 6, 2, 160 as feetInput, inchesInput, poundsInput
     landing = client.post("/index", method="POST", data = { "feetInput" : 6, "inchesInput" : 2, "poundsInput" : 160 })
     html = landing.data.decode()
 
+    # searches for "21.0" and "normal weight" in the page's html
     assert "21.0" in html
     assert "normal weight" in html
         
